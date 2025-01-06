58e4c6b12493ccd2ab1b988e1381cec59f95ed50
pgsto <pgstom9015@gmail.com>
2025-01-06 20:52:10 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="handlerA(),handlerB()">
+    Click me!
+  </button>
+</template>
+
+<script>
+export default {
+  methods:{
+    handlerA(){
+      console.log('A')
+    },
+    handlerB(){
+      console.log('B')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
