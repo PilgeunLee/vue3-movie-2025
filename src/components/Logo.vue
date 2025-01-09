5fcd31918021437fcc54ed1a2d66e70e62c31d94
pgsto <pgstom9015@gmail.com>
2025-01-09 21:59:49 +0900

    Logo add

---
+++ src/components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </router-link>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo{
+  font-family: "Oswald","sans-serif";
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+  &:hover { 
+    color : $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
