e3e69918a62ad8c19b4c5ab38059bad456c03739
pgsto <pgstom9015@gmail.com>
2025-01-18 14:09:28 +0900

    filter_MovieList add

---
+++ src/components/MovieList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <MovieItem 
+        v-for="movie in mevies"
+        :key="movie.imdbID" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieItem from '~/components/MovieItem'
+export default {
+  components:{
+    MovieItem
+  },
+  data(){
+    return{
+      movies:[]
+    }
+  }
+
+}
+
+</script>
+
+<style></style>
