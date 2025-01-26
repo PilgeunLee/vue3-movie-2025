303b64c3d9d79f0fc1d32fe56d1351339bb61ad1
pgsto <pgstom9015@gmail.com>
2025-01-26 13:34:26 +0900

    영화검색코드 리펙토링

--- src/store/movie.js
+++ src/store/movie.js
@@ -45,37 +45,73 @@ export default {
 			// context.state
 			// context.getters
 			// context.commit
-			const { title, type, number, year } = payload //구조분해
-			const OMDB_API_KEY ='7035c60c'
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const {Search, totalResults} = res.data
-			commit('updateState', {
-				movies: _uniqBy(Search, 'imdbID')
-				// message:'hello world',
-				// loading :true
-
-			})
-			console.log(totalResults)
-			console.log(typeof totalResults)
-
-			const total = parseInt(totalResults, 10)
-			const pageLength = Math.ceil(total / 10)
-
-			if(pageLength > 1) {
-				for (let page = 2; page <=pageLength; page += 1){
-					if(page > number / 10){
-						break 
+			//const { title, type, number, year } = payload //구조분해
+			try{
+				const res = await _fetchMovie({
+					...payload,
+					page : 1
+				})
+				const {Search, totalResults} = res.data
+				commit('updateState', {
+					movies: _uniqBy(Search, 'imdbID')
+					// message:'hello world',
+					// loading :true
+	
+				})
+				console.log(totalResults)
+				console.log(typeof totalResults)
+	
+				const total = parseInt(totalResults, 10)
+				const pageLength = Math.ceil(total / 10)
+	
+				if(pageLength > 1) {
+					for (let page = 2; page <=pageLength; page += 1){
+						if(page > (payload.number/ 10)){
+							break 
+						}
+						const res = await _fetchMovie({
+							...payload,
+							page
+						})
+						const{ Search } =res.data
+						commit ('updateState',{
+							movies: [
+								...state.movies,
+								..._uniqBy(Search,'imdbID')
+							]
+						})
 					}
-					const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-					const{ Search } =res.data
-					commit ('updateState',{
-						movies: [
-							...state.movies,
-							..._uniqBy(Search,'imdbID')
-						]
-					})
-				}
+				}	
+			} catch(message){
+				commit('updateState',{
+					movies:[],
+					message
+				})
 			}
+     
 		}
 	}
+}
+
+// _해당파일에서만 사용되는 의미
+function _fetchMovie(payload) {
+	const {title, type, year, page} =payload
+	const OMDB_API_KEY ='7035c60c'
+	const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+
+
+	return new Promise((resolve, reject) =>{
+		axios.get(url)
+			.then(res =>{
+				if(res.data.Error){
+					reject(res.data.Error)
+				}
+				resolve(res)
+			})
+			.catch(err =>{
+				reject(err.message)
+			})
+	})
+
 }
